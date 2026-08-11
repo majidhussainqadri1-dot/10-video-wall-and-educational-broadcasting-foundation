69a1514abce2c118721bc249f7a5447381226b43
majidhussainqadri1-dot <majidhussainqadri1@gmail.com>
2026-08-11 22:52:43 +0500

    Integrate Future 24 tools into File 10 canonical routes

--- video-wall-and-live-broadcasting/templates/route.php
+++ video-wall-and-live-broadcasting/templates/route.php
@@ -2,12 +2,12 @@
 defined('ABSPATH')||exit;
 get_header();
 echo '<div id="primary" class="content-area"><div class="site-main">';
-if(get_query_var('vwlb_video_id'))echo do_shortcode('[vwlb_video]');
-elseif(get_query_var('vwlb_live_id'))echo do_shortcode('[vwlb_live]');
+if(get_query_var('vwlb_video_id'))echo do_shortcode('[vwlb_video][vwlb_future_video_tools]');
+elseif(get_query_var('vwlb_live_id'))echo do_shortcode('[vwlb_live][vwlb_future_live_tools]');
 elseif(get_query_var('vwlb_channel_slug'))echo do_shortcode('[vwlb_channel]');
 elseif(get_query_var('vwlb_podcast_id'))echo do_shortcode('[vwlb_podcast]');
 elseif('studio_video'===get_query_var('vwlb_route'))echo do_shortcode('[vwlb_studio_video]');
-elseif('studio_live'===get_query_var('vwlb_route'))echo do_shortcode('[vwlb_studio_live]');
+elseif('studio_live'===get_query_var('vwlb_route'))echo do_shortcode('[vwlb_studio_live][vwlb_future_production_studio]');
 else echo do_shortcode('[vwlb_wall]');
 echo '</div></div>';
 get_footer();
